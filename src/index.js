f25dcf2a8cec30a99ecf3e986eded6aa713478d0
Nik <minscky.d123@yandex.ru>
2018-09-30 20:48:47 +0300

    Init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+     
+    var stoc;
+    var copystr = str;
+
+    for (var i=0; i<bracketsConfig.length; i++){
+        stoc = bracketsConfig[i][0]+bracketsConfig[i][1];
+     
+        while (copystr.includes(stoc)){
+            i = copystr.indexOf(stoc);
+            copystr = copystr.substring(0, i) + copystr.substring(i + 2);
+            i = -1;
+            }
+         
+    }
+    
+    return (!copystr.length);
+    };
+   
+
+
